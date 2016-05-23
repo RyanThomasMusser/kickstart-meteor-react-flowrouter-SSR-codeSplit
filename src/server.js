71fc3533ba7d3ea66747a5afffa73f4d1220c60c
Guillaume Sabran <sabranguillaume@gmail.com>
2016-05-23 16:36:02 -0400

    make the server fail

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
 import 'TodoApp/server';
+// import TodoApp from 'TodoApp/client/components/TodoApp';
+// console.log('TodoApp', !!TodoApp);
+
+require.ensure(['TodoApp/client/components/TodoApp.jsx'], function(require) {
+  const TodoApp = require('TodoApp/client/components/TodoApp.jsx');
+  console.log('TodoApp', !!TodoApp);
+})
